1368e0ebde43e575b042fbef56ec12e84005c51f
Allysson579 <88337350+Allysson579@users.noreply.github.com>
2021-08-03 19:35:51 -0300

    Add files via upload

---
+++ CARRO CONSUMO.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float resultado (float a, float b)
+{
+    return (a/b);
+}
+
+int main (){
+        float Km, litros, consumo;
+        
+        printf("Qual a quilometragem rodada em KM? ", &a);
+            scanf("%f", & Km);
+        
+        printf("Qual a quantidade de combustível em litros abastecido? ", &b);
+            scanf("%f", & litros);
+            
+             printf("Qual a quantidade de consumida de combustivel? ", &consumo);
+            scanf("%f", & consumo);
+            
+        printf ("A  quantidade consumida pelo carro é: Exibir litros/ km: ", resultado);
+        
+return 0;
+}
+
+- calculo do KM rodado - o consumo do carro e quantidade de combustível que o carro tem (km rodado / pelo consumo do carro que é 15, &resultado);
+- O carro consome 1lt a cada 15km percorrido
+
+"Professor não consigo encaixar essas informações na formula"
